ec83f9644d283b7cd8dbd5446bdaffbadece0ad6
sara <sara.rajabzadeh.1999@gmail.com>
2021-10-17 11:43:25 +0330

    Add simpsons index and type token ratio functions

--- features/lexical.py
+++ features/lexical.py
@@ -77,3 +77,15 @@ def sichel_measure(bot, n, v):
     if n == 0:
         return 0
     return v2 / v
+
+def simpsons_index(bot, n, v):
+    s = sum([1.0 * i * (i - 1) for i in bot.values()])
+    if n == 0 or n == 1:
+        return 0
+    return 1 - (s / (n * (n - 1)))
+
+
+def type_token_ratio(bot, n, v):
+    if n == 0:
+        return 0
+    return v / n
